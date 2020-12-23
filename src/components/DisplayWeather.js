b5530d9134779424f6553956a3b961f6cd0247f7
wiggypig <mr.horne@gmail.com>
2020-12-23 05:23:25 -0600

    converted calls to https

--- src/components/DisplayWeather.js
+++ src/components/DisplayWeather.js
@@ -3,7 +3,7 @@ import "./displayweather.css";
 function DisplayWeather(props) {
   const { data } = props;
   const iconurl =
-    "http://openweathermap.org/img/w/" +
+    "https://openweathermap.org/img/w/" +
     `${data.cod != 404 ? data.weather[0].icon : null}` +
     ".png";
   return (
